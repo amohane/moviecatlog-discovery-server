f92ae1265d15f093a1bd29318efa017c46dfbb02
Ankit <ankit.mohane@ondotsystems.com>
2020-05-13 01:48:04 +0530

    initial commit

---
+++ src/main/java/com/moviecatlog/moviecatlogdiscoveryserver/MoviecatlogDiscoveryServerApplication.java
@@ -0,0 +1,27 @@
+package com.moviecatlog.moviecatlogdiscoveryserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaServer
+@RestController
+@RefreshScope
+public class MoviecatlogDiscoveryServerApplication {
+	@Value("${msg:Hi}")
+	private String msg;
+	@GetMapping("/hi")
+	public String sayHi() {
+		return msg;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecatlogDiscoveryServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviecatlog/moviecatlogdiscoveryserver/MoviecatlogDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviecatlog.moviecatlogdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviecatlogDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
